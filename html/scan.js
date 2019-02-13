634d39719d6155697d90ef3a962ed670af5c8cde
Gerald Doussot <gerald.doussot@nccgroup.trust>
2019-02-13 13:54:43 -0800

    Scanning POC completed.

--- html/scan.js
+++ html/scan.js
@@ -1,6 +1,7 @@
-function timeout(ms, promise) {
+function timeout(ms, promise,controller) {
     return new Promise(function (resolve, reject) {
         setTimeout(function () {
+            controller.abort();
             reject(new Error("timeout"))
         }, ms)
         promise.then(resolve, reject)
@@ -9,10 +10,13 @@ function timeout(ms, promise) {
 
 function scan(targetdata) {
     let sendDate = new Date().getTime();
+    var controller = new AbortController();//NO IE support
+    var signal = controller.signal;
     timeout(100, fetch(`http://${targetdata.address}:${targetdata.port}/`, {
             mode: 'no-cors',
-            credentials: 'omit'
-        }))
+            credentials: 'omit',
+            signal
+        }),controller)
         .then(function (response) {
             let receiveDate = new Date().getTime();
             let result = {
@@ -34,12 +38,13 @@ function scan(targetdata) {
                 "target": targetdata
             };
             postMessage(result);
+//            console.log(`Worker: Sending result: ${result.duration}`);
         })
 
 }
 
 onmessage = function (target) {
- //   console.log(`Worker: Message received from main script: ${target.data.address}:${target.data.port}`);
+//    console.log(`Worker: Message received from main script: ${target.data.address}:${target.data.port}`);
     scan(target.data);
  //   console.log('Worker: Posting message back to main script');
 
